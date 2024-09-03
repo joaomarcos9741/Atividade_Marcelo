e0cad29d07faf344ab5f05e21eb46101334f1b28
joaomarcos9741 <joaommarcosc@gmail.com>
2024-09-03 15:21:33 -0300

    volta rede

--- resources/views/cadastroatv.blade.php
+++ resources/views/cadastroatv.blade.php
@@ -1,24 +1,8 @@
 @extends('layout')
 @section('content')
-<div class="containe">
-    <div class="formatv">
-   <form action="processar.php" method="post" class="atv">
-    <h1 class="h1" >CADASTRO ATIVIDADE </h1>
-        <label for="nome">Tarefa: </label>
-        <input type="text" id="nome" name="nome" required>
-        <br><br>
-        <label for="disciplina">Disciplina:</label>
-        <input type="text" id="disciplina" name="disciplina" required>
-        <br><br>
-        <label for="descrição">Descrição: </label>
-        <input type="text" id="descrição" name="descrição" required>
-        <br><br>
-         <input type="submit" value="Salvar" class="botao">
-
-    </form> 
-   </div>
-
-
+<div class="bodyprincipal" >
+    <div class="retangulo">
+        <h1 class="atv">Aqui estão as suas tarefas pendentes...</h1>
+    </div>      
 </div>
-
 @endsection
